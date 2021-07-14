3796506ffbdd039a5eabeceb51f54294864c8571
Katy <katena.moskvina@bk.ru>
2021-07-14 19:02:33 +0600

    git init and node yarn

---
+++ public_html/bitrix/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <title>DolceVita</title>
+
+    <link rel='stylesheet' href="/dist/css/main.css">
+    <script type="text/javascript" src="/dist/js/main.js"></script>
+</head>
+
+<body>
+    <div class="wrapper">
+        <header class="header"></header>
---
+++ public_html/index.php
@@ -0,0 +1,11 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+?>
+
+<main class="main">
+	<section class="page">dolceVita</section>
+</main>
+
+<?
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
+?>
