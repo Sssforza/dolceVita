27abff6ac73cbb36434280f03bb60911dcd1c685
Ferma Developer <support@fermastudio.ru>
2021-08-06 14:05:34 +0600

    server

---
+++ public_html/urlrewrite.php
@@ -0,0 +1,75 @@
+<?php
+$arUrlRewrite=array (
+  0 => 
+  array (
+    'CONDITION' => '#^\\/?\\/mobileapp/jn\\/(.*)\\/.*#',
+    'RULE' => 'componentName=$1',
+    'ID' => NULL,
+    'PATH' => '/bitrix/services/mobileapp/jn.php',
+    'SORT' => 100,
+  ),
+  4 => 
+  array (
+    'CONDITION' => '#^/e-store/xml_catalog/#',
+    'RULE' => '',
+    'ID' => 'bitrix:catalog',
+    'PATH' => '/e-store/xml_catalog/index.php',
+    'SORT' => 100,
+  ),
+  5 => 
+  array (
+    'CONDITION' => '#^/communication/forum/#',
+    'RULE' => '',
+    'ID' => 'bitrix:forum',
+    'PATH' => '/communication/forum/index.php',
+    'SORT' => 100,
+  ),
+  8 => 
+  array (
+    'CONDITION' => '#^/communication/blog/#',
+    'RULE' => '',
+    'ID' => 'bitrix:blog',
+    'PATH' => '/communication/blog/index.php',
+    'SORT' => 100,
+  ),
+  2 => 
+  array (
+    'CONDITION' => '#^/content/articles/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/content/articles/index.php',
+    'SORT' => 100,
+  ),
+  7 => 
+  array (
+    'CONDITION' => '#^/content/gallery/#',
+    'RULE' => '',
+    'ID' => 'bitrix:photogallery_user',
+    'PATH' => '/content/gallery/index.php',
+    'SORT' => 100,
+  ),
+  6 => 
+  array (
+    'CONDITION' => '#^/content/photo/#',
+    'RULE' => '',
+    'ID' => 'bitrix:photogallery',
+    'PATH' => '/content/photo/index.php',
+    'SORT' => 100,
+  ),
+  3 => 
+  array (
+    'CONDITION' => '#^/content/news/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/content/news/index.php',
+    'SORT' => 100,
+  ),
+  1 => 
+  array (
+    'CONDITION' => '#^/rest/#',
+    'RULE' => '',
+    'ID' => NULL,
+    'PATH' => '/bitrix/services/rest/index.php',
+    'SORT' => 100,
+  ),
+);
