5dcf39ae2b9bfd29ee0a5a45d68c9bf5e46bd7d0
Katy <katena.moskvina@bk.ru>
2021-10-08 10:08:50 +0600

    fix

--- src/assets/js/blocks/_sliders.js
+++ src/assets/js/blocks/_sliders.js
@@ -92,6 +92,14 @@ export function sliderSpecialistsPortfolio () {
 			infinite: false,
 			prevArrow: '<div class="sliderArrow__prev"><svg width="27" height="50" viewBox="0 0 27 50" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M25.7988 1L1.79883 25L25.7988 49" stroke="#7C8F99" stroke-width="2" stroke-linecap="round"/></svg></div>',
 			nextArrow: '<div class="sliderArrow__next"><svg width="28" height="50" viewBox="0 0 28 50" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.79883 49L25.7988 25L1.79883 1" stroke="#7C8F99" stroke-width="2" stroke-linecap="round"/></svg></div>',
+			responsive: [
+				{
+					breakpoint: 1025,
+					settings: {
+						arrows: false,
+					}
+				},
+			]
 		});
 		$('div[data-slide]').click(function(e) {
 			$('div[data-slide]').removeClass('active');
